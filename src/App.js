21db89991f89ca6e69b32f25a2719008a463aaa5
Ahmed <ahmed.mano.am68@gmail.com>
2021-11-10 13:22:16 +0200

    added needed dependencies

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Header from "./components/Header";
 
-import "./app.css";
+import "./App.css";
 
 const App = () => {
   return (
