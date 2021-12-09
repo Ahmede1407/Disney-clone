9c6de706ce088080970ca25a3c303d64465f505b
Ahmed <ahmed.mano.am68@gmail.com>
2021-12-09 21:46:46 +0200

    5.6 Add Recommends Component, 6.0 Create Redux State Slice for Getting Movies, 7.0 Add to Firebase & Display Movies & 08.0 Add all Movie Categories.

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,14 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 
 import userReducer from "../features/User/userSlice";
+import movieReducer from "../features/Movie/movieSlice";
 
 export default configureStore({
   reducer: {
     user: userReducer,
+    movie: movieReducer,
   },
-  middleWare: getDefaultMiddleware({
-    serializableCheck: false,
-  }),
+  // middleWare: getDefaultMiddleware({
+  //   serializableCheck: false,
+  // }),
 });
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,71 @@
-import React from "react";
+/* eslint-disable array-callback-return */
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from "react";
 import styled from "styled-components";
 
 import ImgSlider from "./ImgSlider";
+import NewDisney from "./NewDisney";
+import Originals from "./Originals";
+import Recommends from "./Recommends";
+import Trinding from "./Trending";
 import Viewers from "./Viewers";
 
+import { useDispatch, useSelector } from "react-redux";
+import { setMovies } from "../features/Movie/movieSlice";
+import { selectUserName } from "../features/User/userSlice";
+// const data = require("../data/disneyPlusMoviesData.json");
+import { movies } from "../data/disneyPlusMoviesData.json";
+
 const Home = () => {
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+
+  let recommended = [];
+  let newDisneys = [];
+  let originals = [];
+  let trending = [];
+
+  useEffect(() => {
+    movies.map((movie) => {
+      switch (movie.type) {
+        case "recommend":
+          recommended.push({ id: movie.id, ...movie });
+          break;
+
+        case "new":
+          newDisneys.push({ id: movie.id, ...movie });
+          break;
+
+        case "original":
+          originals.push({ id: movie.id, ...movie });
+          break;
+
+        case "trending":
+          trending.push({ id: movie.id, ...movie });
+          break;
+
+        default:
+      }
+    });
+
+    dispatch(
+      setMovies({
+        recommended: recommended,
+        newDisney: newDisneys,
+        original: originals,
+        trending: trending,
+      })
+    );
+  }, [userName]);
+
   return (
     <Container>
       <ImgSlider />
       <Viewers />
+      <Recommends />
+      <Trinding />
+      <NewDisney />
+      <Originals />
     </Container>
   );
 };
